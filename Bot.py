082f6cbaab13a9c9754d835105c12c8dc4b9b60f
Alimurtadlo16 <ahmadalimurtadloasadillah@gmail.com>
2026-07-01 10:59:02 +0700

    Trial Bot System In Telegram Chat

---
+++ Bot.py
@@ -0,0 +1,17 @@
+class PengeluaranHarian:
+    def __init__(self, name, saldoAwal):
+        self.__name = name
+        self.__saldoSaatIni = saldoAwal
+
+    def penguranganTransaksi(self, expense):
+        self.__saldoSaatIni = self.__saldoSaatIni - expense
+        return self.__saldoSaatIni
+
+myDompetGuweh = None
+
+async def set_saldo_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global myDompetGuweh 
+    nama_user = update.effective_user.first_name
+    angka_input = int(context.args[0])
+    myDompetGuweh = PengeluaranHarian(name=nama_user, saldoAwal=angka_input)
+    await update.message.reply_text(f"Saldo awal {nama_user} diatur sebesar {angka_input}")
---
+++ FinancialBotSystem.py
@@ -0,0 +1,31 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+class FinancialBot:
+    def __init__(self, token: str):
+        # Constructor: Inisialisasi bot dengan token lo
+        self.application = Application.builder().token(token).build()
+        self.setup_handlers()
+
+    async def start_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        # Method buat ngerespon perintah /start
+        await update.message.reply_text(
+            "Halo Ali! Gue Murtadlo_bot. Siap bantu lo pantau keuangan biar nggak boncos lagi. 🚀\n\n"
+            "Gunakan /pake [jumlah] [keperluan] buat catat pengeluaran lo nanti!"
+        )
+
+    def setup_handlers(self):
+        # Daftarkan command ke bot
+        self.application.add_handler(CommandHandler("start", self.start_command))
+
+    def run(self):
+        # Jalankan bot secara real-time (Polling)
+        print("Bot lagi jalan, Li... Coba chat /start di Telegram!")
+        self.application.run_polling()
+
+if __name__ == '__main__':
+    # GANTI STRING DI BAWAH INI PAKE TOKEN LO
+    TOKEN = "8764486282:AAFB5bPYXFbKJPdUp9Y5JmAedDLXpkGBPgU"
+    
+    bot = FinancialBot(TOKEN)
+    bot.run()
