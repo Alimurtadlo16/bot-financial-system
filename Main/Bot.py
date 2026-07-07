ba0ea3d2392b5fa226c0fc46c8b14c9aac87e45a
Alimurtadlo16 <ahmadalimurtadloasadillah@gmail.com>
2026-07-07 09:46:02 +0700

    After some logic we will be shine

--- Main/Bot.py
+++ Main/Bot.py
@@ -7,11 +7,27 @@ class PengeluaranHarian:
         self.__saldoSaatIni = self.__saldoSaatIni - expense
         return self.__saldoSaatIni
 
+
 myDompetGuweh = None
 
+
 async def set_saldo_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    global myDompetGuweh 
+    global myDompetGuweh
     nama_user = update.effective_user.first_name
     angka_input = int(context.args[0])
     myDompetGuweh = PengeluaranHarian(name=nama_user, saldoAwal=angka_input)
-    await update.message.reply_text(f"Saldo awal {nama_user} diatur sebesar {angka_input}")
+    await update.message.reply_text(
+        f"Saldo awal {nama_user} diatur sebesar {angka_input}"
+    )
+
+
+async def keluar_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global myDompetGuweh
+    if myDompetGuweh is None:
+        await update.message.reply_text(
+            "Belum ada saldo yang diatur. Silakan atur saldo terlebih dahulu."
+        )
+        return
+    angka_input = int(context.args[0])
+    saldo_terbaru = myDompetGuweh.penguranganTransaksi(angka_input)
+    await update.message.reply_text(f"Saldo terbaru: {saldo_terbaru}")
