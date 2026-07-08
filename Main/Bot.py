62e261fadbfaa6553a5f8fb977a2f3e8dd4a50ad
Alimurtadlo16 <ahmadalimurtadloasadillah@gmail.com>
2026-07-08 11:11:37 +0700

    Fixing use in other user with other phone

--- Main/Bot.py
+++ Main/Bot.py
@@ -15,14 +15,33 @@ class PengeluaranHarian:
 myDompetGuweh = None
 
 
-async def set_saldo_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    global myDompetGuweh
-    nama_user = update.effective_user.first_name
-    angka_input = int(context.args[0])
-    myDompetGuweh = PengeluaranHarian(name=nama_user, saldoAwal=angka_input)
-    await update.message.reply_text(
-        f"Saldo awal {nama_user} diatur sebesar {angka_input}"
-    )
+# async def set_saldo_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+#    global myDompetGuweh
+#    nama_user = update.effective_user.first_name
+#    angka_input = int(context.args[0])
+#    myDompetGuweh = PengeluaranHarian(name=nama_user, saldoAwal=angka_input)
+#    await update.message.reply_text(
+#        f"Saldo awal {nama_user} diatur sebesar {angka_input}"
+#    )
+
+
+async def set_saldo_command(update, context):
+    nama_user = update.message.from_user.first_name
+
+    if not context.args:
+        await update.message.reply_text(
+            f"Format salah,!!! {nama_user}. Harusnya: /set_saldo [angka]\nContoh: /set_saldo 50000"
+        )
+        return
+
+    try:
+        angka_input = int(context.args[0])
+        await update.message.reply_text(
+            f"Sip! Saldo berhasil diatur ke: Rp {angka_input:,}"
+        )
+
+    except ValueError:
+        await update.message.reply_text("Waduh, yang lo masukin harus angka bulat ya!")
 
 
 async def keluar_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -45,12 +64,10 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 def main():
     TOKEN = "8764486282:AAFB5bPYXFbKJPdUp9Y5JmAedDLXpkGBPgU"
-
     application = Application.builder().token(TOKEN).build()
     application.add_handler(CommandHandler("start", start_command))
     application.add_handler(CommandHandler("set", set_saldo_command))
     application.add_handler(CommandHandler("keluar", keluar_command))
-
     print("Bot lo udah standby, Li! Silakan tes di Telegram...")
     application.run_polling()
 
